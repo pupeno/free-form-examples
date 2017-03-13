b52316c2de151ec4c3f296e2cde209c8c4f86139
J. Pablo Fernández <pupeno@pupeno.com>
2017-03-13 07:57:45 +0000

    More matching order of stuff (more or less non-semantic).

--- src/cljs/free_form_examples/core.cljs
+++ src/cljs/free_form_examples/core.cljs
@@ -7,14 +7,14 @@
             [free-form-examples.routing :as routing]
             free-form-examples.config
             free-form-examples.db
-            free-form-examples.forms.reagent.plain
             free-form-examples.forms.reagent.bootstrap-3
             free-form-examples.forms.reagent.bootstrap-3-horizontal
             free-form-examples.forms.reagent.bootstrap-3-inline
-            free-form-examples.forms.re-frame.plain
+            free-form-examples.forms.reagent.plain
             free-form-examples.forms.re-frame.bootstrap-3
             free-form-examples.forms.re-frame.bootstrap-3-horizontal
-            free-form-examples.forms.re-frame.bootstrap-3-inline))
+            free-form-examples.forms.re-frame.bootstrap-3-inline
+            free-form-examples.forms.re-frame.plain))
 
 (defn mount-root []
   (reagent/render [layout/main-panel]
